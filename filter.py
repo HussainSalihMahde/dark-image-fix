a3dba189cd2e769e5ab8887f8cec2fc9cf00afd8
HussainSalih <45163499+HussainSalihMahde@users.noreply.github.com>
2020-05-19 15:50:27 +0300

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
 
+#Sharpen Filter
 array = [
     [3.14,0,3.14],
     [1,0,1],
     [3.14,0,3.14]
 ]
 
-image = cv2.imread('h.jpg')
+image = cv2.imread('Image Source')
 sharpen_kernel = np.array(array)
 sharpen = cv2.filter2D(image, -1, sharpen_kernel)
 
